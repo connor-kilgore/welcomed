e908877d24de3a31a05fe0f009491fd0403ac65e
Contronn <connor.kilgore@gmail.com>
2023-09-08 15:05:47 -0700

    UI update for Main screen, login, signup, and forgot password.

--- app/src/main/java/org/welcomedhere/welcomed/ui/login/SignupActivity.java
+++ app/src/main/java/org/welcomedhere/welcomed/ui/login/SignupActivity.java
@@ -12,9 +12,11 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
